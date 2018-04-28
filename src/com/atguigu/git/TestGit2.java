d5309b436773d381790f79ab3e573f2f2d57210b
冀鹏旭 <1273692835@qq.com>
2018-04-28 19:50:27 +0800

    测试2   3

---
+++ src/com/atguigu/git/TestGit2.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class TestGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("testgit2");
+
+	}
+
+}
---
+++ src/com/atguigu/git/TestGit3.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class TestGit3 {
+
+	public static void main(String[] args) {
+		System.out.println("testgit3");
+
+	}
+
+}
